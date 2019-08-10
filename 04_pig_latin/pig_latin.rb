7d962989b3624cfea72def347dbb1d61af2602f5
gabytzubaws <gabytzubaws@gmail.com>
2019-08-10 13:44:04 +0300

    Solved this exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,11 @@
-#write your code here
+def translate(words)
+  words.split.map.with_index do |word, i|
+    if word.match?(/^[aeiou]/i)
+      "#{word}ay"
+    else
+      consonants = word.match(/^([^(aeiou)]?qu)|([^(aeiou)]+)/i).to_s
+      "#{word[consonants.length..-1]}#{consonants}ay"
+    end
+  end
+    .join(" ")
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  private
+
+  def titleize(content)
+    all_words = content.split
+    all_words
+      .map.with_index do |word, i|
+      if (word.match(/(^the$)|(^an?$)|(^in$)|(^of$)|(^and$)/) && i != 0 && i != all_words.length - 1)
+        word
+      else
+        word.capitalize
+      end
+    end
+      .join(" ")
+  end
 end
